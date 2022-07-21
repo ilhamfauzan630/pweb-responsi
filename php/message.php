87870dc85045b59e6f2598af1b35dab44b839dc3
ilhamfauzan630 <101925259+ilhamfauzan630@users.noreply.github.com>
2022-07-21 09:32:26 +0700

    menambahkan kodingan php untuk menyimpan data ke file txt

--- php/message.php
+++ php/message.php
@@ -6,11 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web Toko Makanan</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/card.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/card.css">
+    <link rel="stylesheet" href="../css/input.css">
     <style>
         body {
-            background-image: url("assets/food-blur.png");
+            background-image: url("../assets/food-blur.png");
 
 
             background-attachment: fixed;
@@ -18,7 +19,15 @@
             background-repeat: no-repeat;
             background-size: cover;
         }
-        
+        .delivery
+        {
+            padding: 20px;
+            border-radius: 7px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
     </style>
 </head>
 
@@ -27,8 +36,8 @@
         <nav class="navigation">
             <div class="menuToggle"></div>
             <ul>
-                <li class="list active" style="--clr:#2196f3;">
-                    <a href="#">
+                <li class="list" style="--clr:#2196f3;">
+                    <a href="../index.html">
                         <span class="icon">
                             <ion-icon name="home-outline"></ion-icon>
                         </span>
@@ -36,14 +45,14 @@
                     </a>
                 </li>
                 <li class="list" style="--clr:#f44336;">
-                    <a href="php/food.html">
+                    <a href="food.html">
                         <span class="icon">
                             <ion-icon name="fast-food-outline"></ion-icon>
                         </span>
                         <span class="text">Food</span>
                     </a>
                 </li>
-                <li class="list" style="--clr:#0fc70f;">
+                <li class="list active" style="--clr:#0fc70f;">
                     <a href="#">
                         <span class="icon">
                             <ion-icon name="chatbubbles-outline"></ion-icon>
@@ -52,16 +61,16 @@
                     </a>
                 </li>
                 <li class="list" style="--clr:#ffa177;">
-                    <a href="#">
+                    <a href="user.php">
                         <span class="icon">
                             <ion-icon name="person-outline"></ion-icon>
                         </span>
                         <span class="text">About</span>
                     </a>
                 </li>
-                
-                
-                
+
+
+
 
                 <li class="list" style="--clr:#b145e9;">
                     <a href="#">
@@ -76,11 +85,84 @@
     </header>
 
     <main>
+        <div class="delivery">
+            <h2>Order Makanan</h2>
+            <form action="message.php" method="post">
+                <div class="input">
+                    <input type="text" name="nama" id="nama" required><br>
+                    <label for="nama">Nama Lengkap</label>
+                </div>
+                <div class="input">
+                    <input type="text" name="alamat" id="alamat" required><br>
+                    <label for="Alamat">Alamat Rumah</label>
+                </div>
+                <div class="input">
+                    <input type="number" name="nomer" id="nomer"><br>
+                    <label for="nomer">Nomer Telepon</label>
+                </div>
+                
+                <div class="input">
+                    <label for="makanan">Pilih Pesanan</label>
+                    <select name="makanan" id="makanan">
+                        <option value="batagor">Batagor</option>
+                        <option value="bakso">Bakso</option>
+                        <option value="mie">Mie Goreng</option>
+                        <option value="cuanki">Bakso cuanki</option>
+                    </select>
+                </div>
+                <div class="input">
+                    <p>Pedas</p>
+                    <input type="radio" name="radio" id="radio" value="level 1"><label for="lev1">Level 1</label>
+                    <input type="radio" name="radio" id="radio" value="level 2"><label for="lev2">Level 2</label>
+                    <input type="radio" name="radio" id="radio" value="level 3"><label for="lev3">Level 3</label>
+                    <input type="radio" name="radio" id="radio" value="level 4"><label for="lev4">Level 4</label>
+                    <input type="radio" name="radio" id="radio" value="level 5"><label for="lev5">Level 5</label>
+                </div>
+                <div class="submit">
+                    <input type="submit" value="Order">
+                </div>
+            </form>
+            <?php
+            if (isset($_POST['nama'])) {
+                $nama = $_POST['nama'];
+                $alamat = $_POST['alamat'];
+                $nomer = $_POST['nomer'];
+                $makanan = $_POST['makanan'];
+                $radio = $_POST['radio'];
+                $total = 0;
+                if($makanan == "batagor")
+                {
+                    $total = $total + 10000;
+                }
+                else if($makanan == "bakso")
+                {
+                    $total = $total + 15000;
+                }
+                else if($makanan == "mie")
+                {
+                    $total = $total + 20000;
+                }
+                else if($makanan == "cuanki")
+                {
+                    $total = $total + 25000;
+                }
+                echo "<h3>$nama</h3>";
+                echo "<h3>$alamat</h3>";
+                echo "<h3>$nomer</h3>";
+                echo "<h3>$makanan</h3>";
+                echo "<h3>$radio</h3>";
+                echo "<h3>$total</h3>";
+            }
+            $fp = fopen("message.txt", "a+");
+            fwrite($fp, "$nama|$alamat|$nomer|$makanan|$radio|$total\n");
+            fclose($fp);
+            ?>
+        </div>
     </main>
     <footer>
 
     </footer>
-    <script src="js/script.js"></script>
+    <script src="../js/script.js"></script>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
--- php/user.php
+++ php/user.php
@@ -6,11 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web Toko Makanan</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/card.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/card.css">
     <style>
         body {
-            background-image: url("assets/food-blur.png");
+            background-image: url("../assets/food-blur.png");
 
 
             background-attachment: fixed;
@@ -18,7 +18,15 @@
             background-repeat: no-repeat;
             background-size: cover;
         }
-        
+        .user
+        {
+            padding: 20px;
+            border-radius: 7px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
     </style>
 </head>
 
@@ -27,8 +35,8 @@
         <nav class="navigation">
             <div class="menuToggle"></div>
             <ul>
-                <li class="list active" style="--clr:#2196f3;">
-                    <a href="#">
+                <li class="list" style="--clr:#2196f3;">
+                    <a href="../index.html">
                         <span class="icon">
                             <ion-icon name="home-outline"></ion-icon>
                         </span>
@@ -36,7 +44,7 @@
                     </a>
                 </li>
                 <li class="list" style="--clr:#f44336;">
-                    <a href="php/food.html">
+                    <a href="food.html">
                         <span class="icon">
                             <ion-icon name="fast-food-outline"></ion-icon>
                         </span>
@@ -44,14 +52,14 @@
                     </a>
                 </li>
                 <li class="list" style="--clr:#0fc70f;">
-                    <a href="#">
+                    <a href="message.php">
                         <span class="icon">
                             <ion-icon name="chatbubbles-outline"></ion-icon>
                         </span>
                         <span class="text">Messages</span>
                     </a>
                 </li>
-                <li class="list" style="--clr:#ffa177;">
+                <li class="list active" style="--clr:#ffa177;">
                     <a href="#">
                         <span class="icon">
                             <ion-icon name="person-outline"></ion-icon>
@@ -76,11 +84,14 @@
     </header>
 
     <main>
+        <div class="user">
+            <h2>list Pesanan</h2>
+        </div>
     </main>
     <footer>
 
     </footer>
-    <script src="js/script.js"></script>
+    <script src="../js/script.js"></script>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
